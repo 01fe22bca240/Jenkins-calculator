1e93f078fc6d58024892b03761593b31bebafc89
Kapil G Ratan <156279987+01fe22bca240@users.noreply.github.com>
2024-02-20 11:24:01 +0530

    Update Extended_Calculator.java

--- Extended_Calculator.java
+++ Extended_Calculator.java
@@ -1,6 +1,6 @@
 public class Extended_Calculator {
     float square(float operand_1){
-        return (operand_1 * operand_1)
+        return (operand_1  operand_1);
     }
     float cube(float operand_1){
         return (operand_1*operand_1*operand_1);
